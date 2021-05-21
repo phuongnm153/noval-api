f28901e5981e22db14ded193095f3efe0396bf26
ThangKT <thangkt@fpt.com.vn>
2021-05-21 15:30:10 +0700

    Test lint staged

--- src/models/payment.model.js
+++ src/models/payment.model.js
@@ -75,5 +75,4 @@ PaymentSchema.index({
 
 PaymentSchema.plugin(autoIncrement.plugin, { model: 'Payments', startAt: 1 });
 
-//Add some comment
 module.exports = mongoose.model('Payments', PaymentSchema);
